6242208dbca6af900042d9fb60c3f6ba6cdbacd8
euteryu <54965926+euteryu@users.noreply.github.com>
2022-09-01 15:25:18 +0100

    Update keithley_minolta.py

--- src/keithley_minolta.py
+++ src/keithley_minolta.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug 31 15:59:30 2022
-
 @author: gryu
 """
 
 import time
 import serial
 
+
+##  TODO: RAISE EXCEPTION AND CLOSE SERIAL PORT IF ANY WRITE FAILURE OCCUR,
+##        TO PREVENT `SerialException`  ERROR.
 def keithley(voltage):
-    volt = float(voltage)
-    ser_keithley = serial.Serial(port="COM4", baudrate=19200, parity=serial.PARITY_NONE, bytesize=8, stopbits=1)
+    voltf = float(voltage)
+    #voltfe = str(voltf).encode('utf-8')
+    ser_keithley = serial.Serial(port="COM7", baudrate=38400, parity=serial.PARITY_NONE, bytesize=8, stopbits=1)
     
-    ser_keithley.write(b'*RST\r')                       #Reset Keithley
-    ser_keithley.write(b':SOUR:FUNC VOLT\r')            #Set Voltage as SOURCE
-    ser_keithley.write(b':SOUR:VOLT:MODE FIXED\r')      #Fixed Voltage
-    ser_keithley.write(b':SOUR:VOLT:LEV %f\r' %volt)   #Set Voltage from request
-    ser_keithley.write(b':SENS:FUNC "CURRENT"\r')       #Set Current as SENSOR
-    ser_keithley.write(b':FORM:ELEM VOLT, CURR\r')      #Retrieve Voltage and Current only 
-    ser_keithley.write(b':SENS:VOLT:PROT 5\r')          #Cmpl voltage in Volt
-    ser_keithley.write(b':SENS:CURR:PROT 0.5\r')        #Cmpl current in Ampere
-    ser_keithley.write(b':SENS:CURR:NPLC 0.1\r')        #AC noise integrating time
+    ser_keithley.write(b"*RST\r")                           #Reset Keithley
+    ser_keithley.write(b":SOUR:FUNC VOLT\r")                #Set Voltage as SOURCE
+    ser_keithley.write(b":SOUR:VOLT:MODE FIXED\r")          #Fixed Voltage
+    #ser_keithley.write(b":SOUR:VOLT:LEV %f\r" % voltf)    #Set Voltage from request
+    #ser_keithley.write(b":SOUR:VOLT:LEV " + voltfe + "\r")
+    #voltf_fixed = bytes(":SOUR:VOLT:LEV {0}\r".format(voltf))
+    ser_keithley.write(bytes(":SOUR:VOLT:LEV {0}\r".format(voltf), encoding='utf8'))
+
+    ser_keithley.write(b':SENS:FUNC "CURRENT"\r')           #Set Current as SENSOR
+    ser_keithley.write(b':FORM:ELEM VOLT, CURR\r')          #Retrieve Voltage and Current only 
+    ser_keithley.write(b':SENS:VOLT:PROT 5\r')              #Cmpl voltage in Volt
+    ser_keithley.write(b':SENS:CURR:PROT 0.5\r')            #Cmpl current in Ampere
+    ser_keithley.write(b':SENS:CURR:NPLC 0.1\r')            #AC noise integrating time
     ser_keithley.write(b':OUTP ON\r')
     ser_keithley.write(b':READ?\r')
         
@@ -28,10 +35,10 @@ def keithley(voltage):
 
     ser_keithley.write(b':OUTP OFF\r')
         
-    values = [float(i) for i in raw_data.decode('ascii').strip().split(',')]
+    values = [float(i) for i in raw_data.decode('utf-8').strip().split(',')]
     voltage = values[0]
     current = values[1]
-    
+    ser_keithley.close()
     return voltage, current
 
 def Minolta():
@@ -54,10 +61,12 @@ def Minolta():
     return luminance[4:-2]
 
 
+# baudrate:
+# 38400  --  ph-splsim lab
+# 19200  --  ls-100 lab
 for i in range(-12,13):
     voltage = i/10
-    keithley(voltage)
-    Minolta()
-    # time.sleep(0.1)
-    
-    
+    print(keithley(voltage))
+    #print("voltf {0}".format(voltage))
+    #Minolta()
+    #time.sleep(0.1)
